af5f31d0a233575acd666750c7dac634d3ca8dd1
Patric Meyer <patric.meyer@freenet.de>
2019-05-11 16:05:44 +0200

    updates to use Python 3 instead of Python 2

--- action-queryTodayEarnings-MeyPVLogger.py
+++ action-queryTodayEarnings-MeyPVLogger.py
@@ -1,17 +1,43 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import configparser
 from hermes_python.hermes import Hermes
+from hermes_python.ffi.utils import MqttOptions
+from hermes_python.ontology import *
+import io
+
+CONFIGURATION_ENCODING_FORMAT = "utf-8"
+CONFIG_INI = "config.ini"
+
+class SnipsConfigParser(configparser.SafeConfigParser):
+    def to_dict(self):
+        return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
 
 
-def action_wrapper(hermes, intent_message):
+def read_configuration_file(configuration_file):
+    try:
+        with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
+            conf_parser = SnipsConfigParser()
+            conf_parser.readfp(f)
+            return conf_parser.to_dict()
+    except (IOError, configparser.Error) as e:
+        return dict()
+
+def subscribe_intent_callback(hermes, intentMessage):
+    conf = read_configuration_file(CONFIG_INI)
+    action_wrapper(hermes, intentMessage, conf)
+
+
+def action_wrapper(hermes, intentMessage, conf):
     result_sentence = "Hallo"
 
-    current_session_id = intent_message.session_id
+    current_session_id = intentMessage.session_id
     hermes.publish_end_session(current_session_id, result_sentence)
 
 
 if __name__ == "__main__":
-    with Hermes("localhost:1883") as h:
-        h.subscribe_intent("PM_SNIPS:queryTodayEarnings", action_wrapper).start()
-        
+    mqtt_opts = MqttOptions()
+    with Hermes(mqtt_options=mqtt_opts) as h:
+        h.subscribe_intent("PM_SNIPS:queryTodayEarnings", subscribe_intent_callback) \
+         .start()
